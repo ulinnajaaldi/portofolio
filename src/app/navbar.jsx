2e48d532e616a741e135d847d53e9c7ccdd9f026
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-26 23:42:41 +0700

    feat: navbar (not fix)

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -10,52 +10,84 @@ export default function Navbar() {
     window.localStorage.getItem("theme" || "light")
   );
 
-  const toogleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     window.localStorage.setItem("theme", theme);
   }, [theme]);
 
+  const toogleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <>
-      <nav className="flex h-24 flex-wrap items-center justify-between px-8 shadow-md">
-        <div>
-          <h1 className="text-xl font-semibold">Aldilla Ulinnaja</h1>
-          <p>Software Engginer</p>
+      <div className="flex h-24 items-center justify-between px-7 shadow-sm lg:px-14">
+        <div className=" text-accent">
+          <h1 className="text-xl font-medium">Aldilla Ulinnaja</h1>
+          <p className="font-light ">Front-End Software Engginer</p>
         </div>
-        <div className="block transition-transform duration-1000 ease-in-out md:hidden">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="relative z-40 md:hidden "
-          >
+
+        {/* Hamburger Button */}
+        <div className="flex lg:hidden">
+          <button onClick={() => setIsOpen(!isOpen)} className="relative z-30">
             {!isOpen ? (
-              <BiMenuAltRight className="h-10 w-10 fill-current" />
+              <BiMenuAltRight className="h-10 w-10 fill-current text-accent" />
             ) : (
-              <BiX className="h-10 w-10 fill-current" />
+              <BiX className="h-10 w-10 fill-current text-accent" />
             )}
           </button>
         </div>
-        <div className="hidden gap-5 md:flex">
-          <Link href="#home">Home</Link>
-          <Link href="#project">Project</Link>
-          <Link href="#Contact">Contact</Link>
+
+        {/* Navbar Text */}
+        <div className="hidden gap-10  text-base-content lg:flex">
+          <Link href="#home" className="hover:text-accent">
+            About
+          </Link>
+          <Link href="#project" className="hover:text-accent">
+            Project
+          </Link>
+          <Link href="#Contact" className="hover:text-accent">
+            Contact
+          </Link>
+          <button onClick={toogleTheme}>
+            {theme === "dark" ? (
+              <MdLightMode size={25} className="text-yellow-500" />
+            ) : (
+              <MdDarkMode size={25} className="text-yellow-500" />
+            )}
+          </button>
         </div>
-      </nav>
+      </div>
+
       {/* Sidebar */}
       <div
-        className={`w-full overflow-hidden  md:hidden ${
-          isOpen ? "block" : "hidden"
+        className={`fixed top-0 w-full lg:hidden ${
+          !isOpen
+            ? "translate-x-full transition-all duration-150 ease-in-out"
+            : "ease-in-ou translate-x-0 transition-all duration-150 ease-in-out"
         }`}
       >
-        <div className="absolute top-0 h-screen w-full backdrop-blur-sm"></div>
-        <div className="absolute right-0 top-0 z-0 h-screen w-3/4 bg-white drop-shadow-lg">
-          <div className="flex flex-col  p-5 text-neutral">
-            <Link href="#home">Home</Link>
-            <Link href="#project">Project</Link>
-            <Link href="#Contact">Contact</Link>
+        <div className="fixed top-0 z-0 h-screen w-full backdrop-blur-sm "></div>
+        <div className="fixed right-0 top-0 z-10 h-screen w-3/4 bg-base-200 drop-shadow-lg md:w-1/2">
+          <div className="mt-24 flex flex-col items-center justify-center gap-16 text-lg text-base-content">
+            <Link href="#home" className="hover:text-accent">
+              About
+            </Link>
+            <Link href="#project" className="hover:text-accent">
+              Project
+            </Link>
+            <Link href="#Contact" className="hover:text-accent">
+              Contact
+            </Link>
+            <div>
+              <button onClick={toogleTheme}>
+                {theme === "dark" ? (
+                  <MdLightMode size={25} className="text-yellow-500" />
+                ) : (
+                  <MdDarkMode size={25} className="text-yellow-500" />
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </div>
