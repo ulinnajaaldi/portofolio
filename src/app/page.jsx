b560cfa3a4d75df5fa8b5749db730d63d1d0a1dd
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-01-27 23:43:51 +0700

    feat: rebuild pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,16 @@
 export default function Home() {
   return (
-    <div className="h-fit w-full">
-      <div className="flex h-[1900px] flex-col items-center justify-center">
-        <p>Hi, my name is</p>
-        <h1>Aldilla Ulinnaja.</h1>
-        <h2>I build things for the web.</h2>
-        <p>
-          I’m a software engineer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building accessible, human-centered products at Upstatement.
+    <div>
+      <div className="flex h-screen w-full flex-col justify-center leading-relaxed">
+        <p className="ml-1 pb-7 text-base text-accent">Hi, my name is</p>
+        <h1 className="pb-4 text-6xl font-semibold text-base-300">
+          Aldilla Ulinnaja.
+        </h1>
+        <h1 className="pb-8 text-6xl font-semibold">
+          I build things for the web.
+        </h1>
+        <p className="text-md w-[540px]">
+          {`I'm a software engineer specializes in building (and sometimes designing) amazing and new digital experiences. Currently, I am focusing on creating interactive 3d website applications for industrial needs`}
         </p>
       </div>
     </div>
