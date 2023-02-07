d400f4da3a8d4ce5ba192b03cdc8157d82040559
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-02-07 23:50:12 +0700

    style: fix responsive

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full py-10 text-center font-mono text-sm text-primary">
-      Build by Aldilla Ulinnaja. All right reserved.
+    <div className="w-full py-5 text-center font-mono text-[10px] text-primary md:text-sm">
+      Build by Ulinnaja Aldi. All right reserved.
     </div>
   );
 }
