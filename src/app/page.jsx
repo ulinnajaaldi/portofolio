561e39fd2530e769ed6bfb7e37dc8bf2747df7c6
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-02-07 23:48:11 +0700

    feat: add secret image & fix responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,8 @@
-import Dominos from "./dominos";
+import Link from "next/link";
+import Dominos from "./models";
+import Image from "next/image";
+import secret from "../../public/images/secret.webp";
+
 export default function Home() {
   return (
     <div>
@@ -13,22 +17,37 @@ export default function Home() {
           I build things for the web.
         </h1>
         <p className="w-full text-sm text-primary md:w-[540px] md:text-base">
-          {`I'm a software engineer specializes in building (and sometimes designing) amazing and new digital experiences. Currently, I am focusing on creating interactive 3d website applications for industrial needs`}
+          {`I'm a Front-End software engineer specializes in building (and sometimes designing) amazing and new digital experiences. Currently, I am focusing on creating interactive 3d website applications for industrial needs.`}
         </p>
+        <div className="mt-10 text-sm md:mt-16">
+          <Link
+            href="/"
+            className="rounded-[4px] border-2 border-accent px-7 py-5 text-accent transition-all duration-300 hover:bg-accent hover:bg-opacity-10"
+          >
+            View my cv here!
+          </Link>
+        </div>
       </div>
-      <div className="flex h-[650px] w-full flex-col items-center justify-center py-24 text-center">
+      <div className="flex h-[650px] w-full flex-col items-center justify-center py-24 text-center ">
+        <div className="absolute mb-[-430px] mr-[10px] md:mb-[400px] md:mr-[200px] lg:mr-[300px] xl:mr-[480px]">
+          <Image
+            src={secret}
+            alt="secret game"
+            className="h-[150px] w-auto opacity-20 md:h-[200px] lg:h-[250px]"
+          />
+        </div>
         <div className="z-40 ">
-          <h2 className="text-5xl font-bold leading-relaxed text-secondary">
+          <h2 className="pb-2 text-3xl font-bold leading-tight text-secondary md:pb-0 md:text-4xl md:leading-relaxed lg:text-5xl lg:leading-relaxed">
             Delivering modern,
             <br />
             cohensive & instuitive web solution.
           </h2>
-          <p className="text-primary">
+          <p className="text-sm text-primary md:text-base">
             I enjoy designing meaningful experiences for brands & businesses to
             help them serve their target users.
           </p>
         </div>
-        <div className="bg-base-100 absolute z-0 m-auto h-[600px] w-[320px] opacity-25 md:w-[672px] lg:w-[825px] xl:w-[1080px]">
+        <div className="bg-base-100 absolute z-0 m-auto h-[600px] w-[350px] opacity-25 md:w-[672px] lg:w-[825px] xl:w-[1080px]">
           <Dominos />
         </div>
       </div>
