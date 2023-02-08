c06d75b675231f82e3c467a2834f5814a4a61c9b
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-02-09 00:50:52 +0700

    feat: add basic structure file & folder

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex h-screen items-center justify-center text-primary">
+      <p>Ini page About</p>
+    </div>
+  );
+}
---
+++ src/app/projects/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Project() {
+  return (
+    <div className="flex h-screen items-center justify-center text-primary">
+      <p>Ini page Project</p>
+    </div>
+  );
+}
