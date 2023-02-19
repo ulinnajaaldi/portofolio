c397ca5cc9457023753ba062e5c56701cd4020a2
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-02-20 00:01:44 +0700

    fix: sosial media contact onHover

--- src/app/contact.jsx
+++ src/app/contact.jsx
@@ -37,16 +37,19 @@ export default function Contact() {
         <div className="flex flex-col items-center" data-aos="fade-up">
           {sosmeds.map((sosmed, index) => (
             <div
-              className="cursor-pointer px-2 py-3 text-primary transition-all duration-300 hover:-translate-y-1 hover:text-accent"
               key={index}
-              title={sosmed.name}
               data-aos="fade-right"
               data-aos-delay={`${index}00`}
               data-aos-duration="600"
             >
-              <a href={sosmed.link} target="_blank" rel="noreferrer">
-                {sosmed.icon}
-              </a>
+              <div
+                className="cursor-pointer px-2 py-3 text-primary transition-all duration-300 hover:-translate-y-1 hover:text-accent"
+                title={sosmed.name}
+              >
+                <a href={sosmed.link} target="_blank" rel="noreferrer">
+                  {sosmed.icon}
+                </a>
+              </div>
             </div>
           ))}
           <div className="mt-4 h-20 w-[1px] bg-primary"></div>
@@ -54,14 +57,15 @@ export default function Contact() {
       </div>
       <div className="fixed bottom-0 right-0 w-[105px]">
         <div className="flex flex-col items-center" data-aos="fade-up">
-          <a
-            href="mailto:ulinnajaaldi24@gmail.com"
-            className="writ px-2 py-3 font-mono text-xs tracking-[0.075em] text-primary transition-all duration-300 hover:-translate-y-1 hover:text-accent"
-            style={{ writingMode: "vertical-rl" }}
-            data-aos="fade-left"
-          >
-            ulinnajaaldi24@gmail.com
-          </a>
+          <div data-aos="fade-left" data-aos-duration="600">
+            <a
+              href="mailto:ulinnajaaldi24@gmail.com"
+              className="writ px-2 py-3 font-mono text-xs tracking-[0.075em] text-primary transition-all duration-300 hover:-translate-y-1 hover:text-accent"
+              style={{ writingMode: "vertical-rl" }}
+            >
+              ulinnajaaldi24@gmail.com
+            </a>
+          </div>
           <div className="mt-4 h-20 w-[1px] bg-primary"></div>
         </div>
       </div>
