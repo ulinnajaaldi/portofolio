5354fbb190e11d9f6e6bae8e23697b3a25a01f72
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-08-17 03:07:37 +0700

    feat: updating data

--- src/utils/datas.js
+++ src/utils/datas.js
@@ -3,8 +3,6 @@ import htmlIcon from "@icons/html.svg";
 import cssIcon from "@icons/css.svg";
 import jsIcon from "@icons/js.svg";
 import phpIcon from "@icons/php.svg";
-import javaIcon from "@icons/java.svg";
-import pythonIcon from "@icons/python.svg";
 import sassIcon from "@icons/sass.svg";
 import bootstrapIcon from "@icons/bootstrap.svg";
 import tailwindIcon from "@icons/tailwind.svg";
@@ -20,11 +18,15 @@ import figmaIcon from "@icons/figma.svg";
 import gitIcon from "@icons/git.svg";
 import githubIcon from "@icons/github.svg";
 import mysqlIcon from "@icons/mysql.svg";
-import photoshopIcon from "@icons/photoshop.svg";
 import splineIcon from "@icons/spline.svg";
 import vscodeIcon from "@icons/vscode.svg";
 import vite from "@icons/vite.svg";
 import webpackIcon from "@icons/webpack.svg";
+import expressIcon from "@icons/express.svg";
+import mongodbIcon from "@icons/mongodb.svg";
+import typescriptIcon from "@icons/typescript.svg";
+import reactQueryIcon from "@icons/react-query.svg";
+import formikIcon from "@icons/formik.png";
 
 // Project Images
 import mulih from "@images/mulih.webp";
@@ -38,6 +40,9 @@ import portfolio_v1 from "@images/portfolio-v1.webp";
 import bertumbuh from "@images/bertumbuh.webp";
 import coffe_bean from "@images/coffe-bean.webp";
 import dapbunda from "@images/dapbunda.webp";
+import hpcjapan from "@images/hpc-japan.webp";
+import ceritain from "@images/ceritain.webp";
+import pinarak_resto from "@images/pinarak_resto.webp";
 
 // Gif Images
 import dapbundaGif from "@gif/dapbunda.gif";
@@ -46,48 +51,56 @@ import portfolioGif from "@gif/portfolio.gif";
 import notesGif from "@gif/notes.gif";
 import abinawaGif from "@gif/abinawa.gif";
 import bookshelfGif from "@gif/bookshelf.gif";
+import sakurajapanGif from "@gif/sakura-japan.gif";
+import ceritainGif from "@gif/ceritain.gif";
+import pinarakrestoGif from "@gif/pinarak-resto.gif";
 
 const tools = {
   Git: { name: "Git", src: gitIcon, level: "Version control" },
   Github: { name: "Github", src: githubIcon, level: "Git hosting" },
   NPM: { name: "NPM", src: npmIcon, level: "Package manager" },
-  Yarn: { name: "Yarn", src: yarnIcon, level: "Dependency tool" },
+  Yarn: { name: "Yarn", src: yarnIcon, level: "Package manager" },
   Vite: { name: "Vite", src: vite, level: "Build tool" },
   VSCode: { name: "VSCode", src: vscodeIcon, level: "Code editor" },
-  MySQL: { name: "MySQL", src: mysqlIcon, level: "Database" },
   Figma: { name: "Figma", src: figmaIcon, level: "UIUX Design tool" },
   Blender: { name: "Blender", src: blenderIcon, level: "3D software" },
   Spline: { name: "Spline", src: splineIcon, level: "Web 3D design" },
-  Photoshop: { name: "Photoshop", src: photoshopIcon, level: "Image editor" },
 };
 
 const stacks = {
   HTML: { name: "HTML", src: htmlIcon, level: "Advanced" },
   CSS: { name: "CSS", src: cssIcon, level: "Advanced" },
   SASS: { name: "SASS", src: sassIcon, level: "Advanced" },
-  JS: { name: "Javascript", src: jsIcon, level: "Advanced" },
   PHP: { name: "PHP", src: phpIcon, level: "Intermediate" },
-  Java: { name: "Java", src: javaIcon, level: "Beginner" },
-  Python: { name: "Python", src: pythonIcon, level: "Beginner" },
+  JS: { name: "Javascript", src: jsIcon, level: "Advanced" },
+  Typescript: { name: "Typescript", src: typescriptIcon, level: "Beginer" },
   Webpack: { name: "Webpack", src: webpackIcon, level: "Advanced" },
   React: { name: "ReactJS", src: reactIcon, level: "Advanced" },
-  NextJS: { name: "NextJS", src: nextjsIcon, level: "Intermediate" },
+  NextJS: { name: "NextJS", src: nextjsIcon, level: "Advanced" },
+  ReactQuery: {
+    name: "React Query",
+    src: reactQueryIcon,
+    level: "Intermediate",
+  },
+  Formik: { name: "Formik", src: formikIcon, level: "Intermediate" },
   ThreeJS: {
     name: "ThreeJS",
     src: threejsIcon,
     level: "Use Spline Tools",
   },
   Laravel: { name: "Laravel", src: laravelIcon, level: "Beginer" },
-  NodeJS: { name: "NodeJS", src: nodejsIcon, level: "Beginner" },
+  Express: { name: "ExpressJS", src: expressIcon, level: "Beginner" },
   Bootstrap: { name: "Bootstrap", src: bootstrapIcon, level: "Intermediate" },
   Tailwind: { name: "TailwindCSS", src: tailwindIcon, level: "Advanced" },
+  MySQL: { name: "MySQL", src: mysqlIcon, level: "Intermediate" },
+  MongoDB: { name: "MongoDB", src: mongodbIcon, level: "Beginer" },
 };
 
 const projects = [
   {
     id: 1,
     name: "Mulih!",
-    desc: "My first project was created using HTML, CSS, and JavaScript. This website was created to fulfill the final assignment for the Belajar Dasar Pemrograman Web course from Dicoding.",
+    desc: "My first project was created using HTML, CSS, and JavaScript. This website was created to fulfill the final assignment for the Basics of Web Programming course from Dicoding ⭐5.",
     image: mulih,
     web: "https://mulih.netlify.app/",
     repo: "https://github.com/ulinnajaaldi/Dicoding-Mulih",
@@ -107,7 +120,7 @@ const projects = [
   {
     id: 3,
     name: "Bookshelf",
-    desc: "This is a website that allows users to keep track of books they have read or plan to read, utilizing DOM modification and CRUD operations with data storage in LocalStorage.",
+    desc: "This is a website that allows users to keep track of books they have read or plan to read, utilizing DOM modification and CRUD operations with data storage in LocalStorage ⭐5.",
     image: bookshelf,
     web: "https://bookshelf-js.vercel.app",
     repo: "https://github.com/ulinnajaaldi/Bookshelf-JS",
@@ -147,7 +160,7 @@ const projects = [
   {
     id: 7,
     name: "Notes",
-    desc: "Welcome to Notes app! With this app, you can create, edit, delete and organize your notes. You can also sign up and log in to access your notes. This app I made for complete the Dicoding React Fundamental course.",
+    desc: "Welcome to Notes app! With this app, you can create, edit, delete and organize your notes. You can also sign up and log in to access your notes. This app I made for complete the Dicoding React Fundamental course ⭐5.",
     image: notes,
     web: "https://notes-app-api-db-dicoding.vercel.app",
     repo: "https://github.com/ulinnajaaldi/Notes",
@@ -157,7 +170,7 @@ const projects = [
   {
     id: 8,
     name: "Portfolio",
-    desc: "My first personal portfolio!! I designed it by combining different ideas from various sources. It also has a secret game that you can play on your desktop. Can you find it?",
+    desc: "My first personal portfolio!! I designed it by combining different ideas from various sources. It also has a secret game that you can play on your desktop. Can you find it? 🛸",
     image: portfolio_v1,
     web: "https://ulinnaja-aldi.vercel.app/",
     repo: "https://github.com/ulinnajaaldi/portofolio",
@@ -193,6 +206,36 @@ const projects = [
     repo: "https://github.com/ulinnajaaldi/dicoding-dapbunda-fe-fundamental",
     stack: "Webpack, Axios, TailwindCSS, DaisyUI, AOS",
     gif: dapbundaGif,
+  },
+  {
+    id: 12,
+    name: "Sakura Japan HPC",
+    desc: "This website was crafted during my role as a speaker at Himatif Portable Club (HPC) 2023, aimed at introducing basic web development. If you're keen on learning how to build this site, please visit the project repository 🚀",
+    image: hpcjapan,
+    web: "https://sakura-japan-hpc.vercel.app/",
+    repo: "https://github.com/ulinnajaaldi/sakura-japan-hpc",
+    stack: "HTML, Javascript, TailwindCSS, Custom Element Javascript",
+    gif: sakurajapanGif,
+  },
+  {
+    id: 13,
+    name: "Ceritain!",
+    desc: "This website was developed as part of the final assignment for the Intermediate Front-End Web Tools course by Dicoding ⭐4. It is designed to enable users to share their stories, photos, and current location.",
+    image: ceritain,
+    web: "https://ceritain-app.web.app/",
+    repo: "https://github.com/ulinnajaaldi/story-app-fe-intermediate",
+    stack: "Javascript, Webpack, Axios, LitElement, Saas, Bootstrap, Firebase",
+    gif: ceritainGif,
+  },
+  {
+    id: 14,
+    name: "Pinarak Resto",
+    desc: "This website was developed as part of the final assignment for the Front-End Web Developer Expert course by Dicoding ⭐5, and this website serves as an informational platform where users can comment on each restaurant.",
+    image: pinarak_resto,
+    web: "https://pinarak-resto.netlify.app/",
+    repo: "https://github.com/ulinnajaaldi/dicoding-pinarak-fe-expert",
+    stack: "Javascript, Webpack, PWA, IDB, CodeceptJs, Jasmine, Karma, Sass",
+    gif: pinarakrestoGif,
   },
 ];
 
