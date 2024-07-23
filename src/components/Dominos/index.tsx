2d4bda8d200a1265835a952e7c34e3d0d64ff9d5
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2024-07-23 21:23:11 +0700

    chore: optimize lazy loading for dominos component

--- src/components/Dominos/index.tsx
+++ src/components/Dominos/index.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import { type FC, useState } from "react";
-import Spline from "@splinetool/react-spline";
+import { lazy, Suspense, useState, FC } from "react";
+const Spline = lazy(() => import("@splinetool/react-spline"));
 import { LoadingSpinner } from "@/components";
 
 const Dominos: FC = () => {
   const [loading, setLoading] = useState(true);
 
   return (
-    <>
+    <Suspense
+      fallback={
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <LoadingSpinner />
+        </div>
+      }
+    >
       <Spline
         onLoad={() => setLoading(false)}
         scene="https://prod.spline.design/BJjm9TZpZw1fn-BD/scene.splinecode"
@@ -26,7 +40,7 @@ const Dominos: FC = () => {
           <LoadingSpinner />
         </div>
       )}
-    </>
+    </Suspense>
   );
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -7,7 +7,9 @@ export const Footer = dynamic(() => import("./Footer"));
 export const Navbar = dynamic(() => import("./Navbar"), {
   ssr: false,
 });
-export const Dominos = dynamic(() => import("./Dominos"));
+export const Dominos = dynamic(() => import("./Dominos"), {
+  ssr: false,
+});
 export const LoadingSpinner = dynamic(() => import("./LoadingSpinner"));
 export const BaseLayout = dynamic(() => import("./Layout/BaseLayout"), {
   ssr: false,
